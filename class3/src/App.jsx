7331c96cb6ee7865c748d15221bdd91b5c3f7f66
naitik-cmd <naitikchs16@gmail.com>
2026-01-08 13:25:57 +0530

    function call on onclick

--- class3/src/App.jsx
+++ class3/src/App.jsx
@@ -14,14 +14,23 @@ const App = () => {
     age: 19,
     sex: 'female'
   };
-
+  function abc(){
+    console.log('Button is clicked!');
+  }
   return (
     <div>
       <Navbar title='Naitik' color='blue' links={['geg', 'hello', 'Hnfs', 'hie']}/>
       <Navbar title='Unnati' color='pink' links={['geg', 'hello', 'Hnfs', 'hie']}/>
       <Navbar title='Ankit' color='brown'links={['geg', 'hello', 'Hnfs', 'hie']}/>
       {user1.sex =='male'?<Men/>:<Women/>}
+      <button
+        onClick={abc}
+        className='bg-red-500 text-white px-4 py-2 rounded block mx-auto mt-4 hover:bg-red-600 transition'>
+        Click here to Download
+      </button>
+
     </div>
+    
 
   )
 }

57b3bca16f181b2ccca5ce65a302eb7e51134c9b
naitik-cmd <naitikchs16@gmail.com>
2026-01-10 02:12:33 +0530

    card-project UI and two way binding

--- class7/src/App.jsx
+++ class7/src/App.jsx
@@ -1,11 +1,67 @@
 import React from 'react'
+import {useState} from 'react'
 
 const App = () => {
+  const [username, setUsername] = useState('');
+  const [imageURL, setImageURL] = useState('');
+  const [userRole, setUserRole] = useState('');
+  const [userDesc, setUserDesc] = useState('');
+  const [allUsers, setAllUsers] = useState('');
+  const submitHandler=(e)=>{
+    e.preventDefault();
+    const oldUsers=[...allUsers];
+    oldUsers.push({username,imageURL,userRole,userDesc});
+    console.log(oldUsers);
+    setAllUsers(oldUsers);
+    setUsername('');
+    setImageURL('');
+    setUserRole('');
+    setUserDesc('');
+  }
   return (
-    <div className='h-screen w-screen bg-black text-white'>
-      <h1 >Naitik Chitranshh</h1>
+    <div className='h-screen bg-black text-white'>
+      <form onSubmit={(e)=>{
+        submitHandler(e);
+      }} className='px-2 py-10 flex flex-wrap justify-center'>
+        <input 
+        value={username} 
+        onChange={(e)=>{
+          setUsername(e.target.value)
+        }}
+        className='border-2 text-xl font-semibold px-5 py-2 rounded m-2 w-[45%]'
+         type="text" 
+         
+         placeholder='Enter your name' />
+        <input 
+        value={imageURL}
+        onChange={(e)=>{
+          setImageURL(e.target.value);
+        }}
+        className='border-2 text-xl font-semibold px-5 py-2 rounded m-2 w-[45%]'
+         type="text" 
+         placeholder='Image URL' />
+        <input 
+        value={userRole}
+        onChange={(e)=>{
+          setUserRole(e.target.value);
+        }}
+        className='border-2 text-xl font-semibold px-5 py-2 rounded m-2 w-[45%]'
+         type="text" 
+         placeholder='Enter Role' />
+        <input 
+        value={userDesc}
+        onChange={(e)=>{
+          setUserDesc(e.target.value);
+        }}
+        className='border-2 text-xl font-semibold px-5 py-2 rounded m-2 w-[45%]'
+         type="text" 
+         placeholder='Enter Description' />
+        <button className='px-5 py-2 active:scale-95 cursor-pointer bg-emerald-700 rounded m-2 w-[92%]'>Create User</button>
+
+      </form>
     </div>
   )
 }
 
 export default App
+

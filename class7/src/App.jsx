222234bf94f0ed1cf72bbf5e15d0abd65c3b98e0
naitik-cmd <naitikchs16@gmail.com>
2026-01-10 00:51:26 +0530

    card project two way binding

---
+++ class7/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-screen bg-black text-white'>
+      <h1 >Naitik Chitranshh</h1>
+    </div>
+  )
+}
+
+export default App

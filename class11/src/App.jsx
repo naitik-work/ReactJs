c39ff1005e866509484c114f783afd182c957fa1
naitik-cmd <naitikchs16@gmail.com>
2026-01-14 00:42:46 +0530

    Learning concept of routing

---
+++ class11/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-6xl underline text-semibold absolute top-1/2 left-1/2 -translate-1/2'>Hi, Naitik</h1>
+    </div>
+  )
+}
+
+export default App

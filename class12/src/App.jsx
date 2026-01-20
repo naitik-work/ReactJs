43b45d9da547c2b8ffe0d709fe431eacc0b4a7c5
naitik-cmd <naitikchs16@gmail.com>
2026-01-21 00:29:52 +0530

    useNavigate function and its usage by importing footer

--- class12/src/App.jsx
+++ class12/src/App.jsx
@@ -4,16 +4,26 @@ import { Route,Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Courses from './pages/Courses'
+import Kodr from './pages/Kodr'
+import Kodex from './pages/kodex'
+import Footer from './components/Footer'
+
 const App = () => {
+
+  
   return (
     <div>
       <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/about' element={<About/>}></Route>
-        <Route path='/courses' element={<Courses/>}></Route>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/about' element={<About/>}></Route>
+      <Route path='/courses' element={<Courses/>}>
+        <Route path='/courses/kodr' element={<Kodr/>}></Route>
+        <Route path='/courses/kodex' element={<Kodex/>}></Route>
+      </Route>
 
-      </Routes>
+    </Routes>
+    <Footer/>
     </div>
   )
 }
--- class12/src/components/Navbar.jsx
+++ class12/src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link,NavLink} from 'react-router-dom'
-import Kodr from '../pages/Kodr'
-import Kodex from '../pages/kodex'
+
 import Courses from '../pages/Courses'
 import About from '../pages/About'
 import Home from '../pages/Home'
@@ -9,7 +8,7 @@ import { Route,Routes } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className='nav'>
-      {/* <NavLink to='/'
+      <NavLink to='/'
       style={({isActive})=>({
         color: isActive? 'red' : 'white'
       })}
@@ -23,19 +22,10 @@ const Navbar = () => {
       style={({isActive})=>({
         color: isActive? 'red' : 'white'
       })}
-      >Courses</NavLink> */}
+      >Courses</NavLink>
   
     
-    <Routes>
-      <Route path='/' element={<Home/>}></Route>
-      <Route path='/courses' element={<Courses/>}></Route>
-      <Route path='/about' element={<About/>}></Route>
-      <Route path='/courses' element={<Courses/>}>
-        <Route path='/courses/kodr' element={<Kodr/>}></Route>
-        <Route path='/courses/kodex' element={<Kodex/>}></Route>
-      </Route>
 
-    </Routes>
     </div>
   )
 }

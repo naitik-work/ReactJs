f863a5c5ad431a3742b9c1d5b1b4b573a3d8e9c6
naitik-cmd <naitikchs16@gmail.com>
2026-01-20 23:54:38 +0530

    Introduction to outlook

--- class12/src/components/Navbar.jsx
+++ class12/src/components/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { Link,NavLink} from 'react-router-dom'
+import Kodr from '../pages/Kodr'
+import Kodex from '../pages/kodex'
+import Courses from '../pages/Courses'
+import About from '../pages/About'
+import Home from '../pages/Home'
+import { Route,Routes } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className='nav'>
-      <NavLink to='/'
+      {/* <NavLink to='/'
       style={({isActive})=>({
         color: isActive? 'red' : 'white'
       })}
@@ -17,9 +23,22 @@ const Navbar = () => {
       style={({isActive})=>({
         color: isActive? 'red' : 'white'
       })}
-      >Courses</NavLink>
+      >Courses</NavLink> */}
+  
+    
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/courses' element={<Courses/>}></Route>
+      <Route path='/about' element={<About/>}></Route>
+      <Route path='/courses' element={<Courses/>}>
+        <Route path='/courses/kodr' element={<Kodr/>}></Route>
+        <Route path='/courses/kodex' element={<Kodex/>}></Route>
+      </Route>
+
+    </Routes>
     </div>
   )
 }
 
 export default Navbar
+
--- class12/src/pages/Courses.jsx
+++ class12/src/pages/Courses.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import { Outlet } from 'react-router-dom'
 const Courses = () => {
   return (
     <div>
       <h1>All courses...</h1>
+
+      <Outlet/>
     </div>
   )
 }
